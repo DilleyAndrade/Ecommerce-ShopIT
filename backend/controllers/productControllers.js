b7ca99b2c7ec801f895b1aeada92a384b48287b1
Dilley Andrade <dilleyandrade@gmail.com>
2025-01-19 01:14:56 -0300

    feat: display all products

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,16 +1,19 @@
 import Product from "../models/productModel.js";
 
 export const getProducts = async (req, res) => {
+  const products = await Product.find()
   res.status(200).json({
     message: "All products",
+    products
   });
 };
 
-export const newProduct =  async(req, res) =>{
-  const productToAdd = await Product.create(req.body)
-  
+//Create product
+export const newProduct = async (req, res) => {
+  const productToAdd = await Product.create(req.body);
+
   res.status(201).json({
-    message: 'Product added',
-    productToAdd
-  })
-}
+    message: "Product added",
+    productToAdd,
+  });
+};
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,7 +4,6 @@ import { newProduct, getProducts } from "../controllers/productControllers.js";
 const router = express.Router();
 
 router.route("/products").get(getProducts);
-
 router.route("/admin/products").post(newProduct);
 
 export default router;
