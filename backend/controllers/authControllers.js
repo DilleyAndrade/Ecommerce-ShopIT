dcd23b9d50392170fff9ea2c4bf594f40585d5fb
Dilley Andrade <dilleyandrade@gmail.com>
2025-01-23 06:13:41 -0300

    feat: login user

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,6 +1,8 @@
 import catchAsyncErrors from "../middlewares/catchAsyncErrors.js";
 import User from "../models/user.js";
+import ErrorHandler from "../util/errorHandler.js";
 
+//Registrar usuário
 export const registerUser = catchAsyncErrors(async (req, res, next) => {
   const { name, email, password } = req.body;
 
@@ -10,9 +12,38 @@ export const registerUser = catchAsyncErrors(async (req, res, next) => {
     password,
   });
 
-  const token = user.getJwtToken()
+  const token = user.getJwtToken();
 
   res.status(201).json({
-    token
+    token,
+  });
+});
+
+//Login usuário
+export const loginUser = catchAsyncErrors(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new ErrorHandler("Please enter email & password", 400));
+  }
+
+  //Encontrar usuário no banco de dados
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return next(new ErrorHandler("Invalid email or password", 401));
+  }
+
+  //Verificar se o password está correto
+  const isPasswordMatched = await user.comparePassword(password);
+
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Invalid email or password", 401));
+  }
+
+  const token = user.getJwtToken();
+
+  res.status(200).json({
+    token,
   });
 });
