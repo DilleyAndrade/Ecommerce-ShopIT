ab3dda58de3f90505bbd014f0ac6456d9d06938d
Dilley Andrade <dilleyandrade@gmail.com>
2025-01-23 05:35:08 -0300

    feat: generate json web token

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema(
   {
@@ -43,5 +44,12 @@ userSchema.pre("save", async function (next) {
   //Se for modificada, encriptar antes de salvar
   this.password = await bcrypt.hash(this.password, 10); //O 10 representa o poder de encriptação
 });
+
+//Retorna o Json Web Token
+userSchema.methods.getJwtToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE_TIME,
+  });
+};
 
 export default mongoose.model("User", userSchema);
